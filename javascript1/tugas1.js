a9ab652ac86a31d21356b2a7b2dfdd2208ed02c7
gbrnchairil23 <114551265+gbrnchairil23@users.noreply.github.com>
2024-11-07 14:08:17 +0700

    JS Fundamental

---
+++ javascript1/tugas1.js
@@ -0,0 +1,25 @@
+//string
+let name = "Gibran";
+console.log(name);
+alert(
+    `Hello, ${name}.`
+);
+
+
+//boolean
+let haveAccount = true;
+console.log("Successfully Logged In", haveAccount);
+
+let nothaveAccount = false;
+console.log("Unable to Log in", nothaveAccount);
+
+//number
+let number = 23;
+alert(number);
+
+//array
+let student = ["Joy", "Irene", "Seulgi", "Wendy"]
+console.log(student)
+student.push("Yeri");
+
+//Object
