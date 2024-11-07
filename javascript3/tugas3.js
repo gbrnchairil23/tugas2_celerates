26c81dda50abf9c305dec7bc50be8bd0a09e9a0f
gbrnchairil23 <114551265+gbrnchairil23@users.noreply.github.com>
2024-11-07 14:20:30 +0700

    JS Control Flow

---
+++ javascript3/tugas3.js
@@ -0,0 +1,18 @@
+
+//soal1
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
+
+//soal2
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
+
+//soal 3
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
+
