3d93ec964ba7acb315835c1bebd10b838734d2e4
gbrnchairil23 <114551265+gbrnchairil23@users.noreply.github.com>
2024-11-07 14:14:19 +0700

    JS Function

---
+++ javascript2/tugas2.js
@@ -0,0 +1,30 @@
+// Soal nomor 1
+function printHeart() {
+    console.log("<3");
+}
+printHeart();
+
+
+// Soal nomor 2
+function multiply(a, b) {
+    return a * b;
+}
+const result = multiply(5, 3);
+console.log(result);
+
+
+//Soal nomor 3
+let animal = "Giant Pacific Octopus"; 
+function observe(){ 
+    let animal = "Pajama Squid"; 
+    console.log(animal);
+} 
+observe();
+
+
+//soal 4
+const sayHello = (name) => `Hello ${name}`;
+
+console.log(sayHello("Yuna")); 
+console.log(sayHello("Ryujin")); 
+
